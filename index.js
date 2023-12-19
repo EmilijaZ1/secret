a922cc799315dea5420bbf8d1ad07edf8c575ac1
Emilija <43810084+EmilijaZ1@users.noreply.github.com>
2023-12-19 12:33:09 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+//To see how the final website should work, run "node solution.js".
+//Make sure you have installed all the dependencies with "npm i".
+//The password is ILoveProgramming
+
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const mypassword = "ILoveProgramming";
+var userIsAuthorised = false;
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+function passwordCheck(req, res, next) {
+    const password = req.body["password"];
+    if (password === "ILoveProgramming") {
+      userIsAuthorised = true;
+    }
+    next();
+  }
+
+app.use(passwordCheck);
+
+
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", (req, res) => {
+
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.redirect("/");
+  }
+
+//   const userinput = req.body.password;
+//   if (userinput === mypassword){
+//     res.sendFile(__dirname + "/public/secret.html");
+//   }
+//   else {
+//     res.redirect("/");
+//   }
+  
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
